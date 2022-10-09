82c6eb0306556b8ca20f8450c4203cd7f856fa23
Brandon Kim <105524304+ENC0D3D-WasTaken@users.noreply.github.com>
2022-10-08 18:40:28 -0600

    Update trade.py

--- commands/trade.py
+++ commands/trade.py
@@ -4,7 +4,7 @@ from main import *
 async def trade(ctx):
     user = getUser(ctx.message.author.id)
     if user is not False:
-        ctx.send(f'{user.name}, your balance is: {user.balance}BSD.')
+        ctx.send(f'{user.name}, your balance is: {user.balance}BSD. Input the user ID of who you want it sent to.')
     else:
         msg = await ctx.send(f'You are not a part of the tycoon, {user.name}')
         deleteMessage(msg)
